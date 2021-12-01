a335410bb5a20b55ba56d5a1c181b5c44879d0c2
Aaro Alhainen <aaro.alhainen@giosg.com>
2021-12-01 22:56:01 +0200

    Day 1

---
+++ src/advent_of_code/core.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code.core
+  (:gen-class)
+  (:require [advent-of-code.days.day1 :as day1]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+  (day1/solve))
